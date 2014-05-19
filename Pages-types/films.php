a935f04e63afb21e6c07424f4c8101ef59c3ad2e
PETILLON-Sebastien <petillon.sebastien@gmail.com>
2014-05-19 19:21:15 +0200

    Recherche ameliorations CSS pour les films (PIHMC-6)

--- Pages-types/films.php
+++ Pages-types/films.php
@@ -19,6 +19,100 @@ require_once("barre-recherche.php");
 
 <section id="films">
 
+
+	<article class="film">
+	<aside><img src="img/1.jpg" /></aside>
+	<header> Transformers 3	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/2.jpg" /></aside>
+	<header> Lord of the Rings	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/3.jpg" /></aside>
+	<header> Matrix </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/4.jpg" /></aside>
+	<header> Marsupilami </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/5.jpg" /></aside>
+	<header> Superman - Man of Steel </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/6.jpg" /></aside>
+	<header> KIN-DZA-DZA </header>
+	</article>
+	
+	
+	<article class="film">
+	<aside><img src="img/1.jpg" /></aside>
+	<header> Transformers 3	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/2.jpg" /></aside>
+	<header> Lord of the Rings	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/3.jpg" /></aside>
+	<header> Matrix </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/4.jpg" /></aside>
+	<header> Marsupilami </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/5.jpg" /></aside>
+	<header> Superman - Man of Steel </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/6.jpg" /></aside>
+	<header> KIN-DZA-DZA </header>
+	</article>
+	
+	
+	<article class="film">
+	<aside><img src="img/1.jpg" /></aside>
+	<header> Transformers 3	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/2.jpg" /></aside>
+	<header> Lord of the Rings	</header>
+	</article>
+
+	<article class="film">
+	<aside><img src="img/3.jpg" /></aside>
+	<header> Matrix </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/4.jpg" /></aside>
+	<header> Marsupilami </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/5.jpg" /></aside>
+	<header> Superman - Man of Steel </header>
+	</article>
+	
+	<article class="film">
+	<aside><img src="img/6.jpg" /></aside>
+	<header> KIN-DZA-DZA </header>
+	</article>
+	
+	
 	<article class="film">
 	<aside><img src="img/1.jpg" /></aside>
 	<header> Transformers 3	</header>
