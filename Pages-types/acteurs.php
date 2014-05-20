80d26f0e197935d089ea2f3ede3d10cc39f678f9
PETILLON-Sebastien <petillon.sebastien@gmail.com>
2014-05-20 09:26:39 +0200

    Travail sur acteurs (PIHMC-6),(PIHMC-5)

---
+++ Pages-types/acteurs.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+
+<?php
+
+require_once("header.php");
+
+?>
+
+<body>
+
+<?php
+
+require_once("banniere.php");
+require_once("menu.php");
+require_once("barre-recherche.php");
+
+?>
+
+<section id="acteurs">
+
+
+	<article class="acteur">
+	<aside><img src="img/a.jpg" /></aside>
+	<header> Megane Fox	</header>
+	</article>
+
+	<article class="acteur">
+	<aside><img src="img/b.jpg" /></aside>
+	<header> Liam Neeson </header>
+	</article>
+
+	<article class="acteur">
+	<aside><img src="img/c.jpg" /></aside>
+	<header> Alain Chabat </header>
+	</article>
+	
+	<article class="acteur">
+	<aside><img src="img/d.jpg" /></aside>
+	<header> Scarlett Johansson </header>
+	</article>
+	
+	<article class="acteur">
+	<aside><img src="img/e.jpg" /></aside>
+	<header> Mila Kunis </header>
+	</article>
+	
+	<article class="acteur">
+	<aside><img src="img/f.jpg" /></aside>
+	<header> Ewan McGregor </header>
+	</article>
+
+	
+</section>
+
+</body>
+
+</html>
